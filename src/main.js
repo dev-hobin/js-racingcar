4808ae7fc0786d87fe46c75e03fc6e5d8ee487da
Hobin <66239735+dev-hobin@users.noreply.github.com>
2025-08-28 21:28:03 +0900

    feat: readline API 코드 참고하여 입출력 구현하기

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-console.log('main')
+import { readLineAsync } from './utils/readLineAsync.js';
+
+play();
+
+async function play() {
+  const name = await readLineAsync('자동차 이름을 입력하세요 > ');
+  console.log(`자동차 이름은 ${name}입니다.`);
+}
