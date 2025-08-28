6734e889baf2f2502aea520e875a7826aaa0b653
Hobin <66239735+dev-hobin@users.noreply.github.com>
2025-08-28 21:38:06 +0900

    feat: 자동차 경주는 5회로 고정하여 진행하고 자동차는 1회당 한칸씩 전진하기

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import { readLineAsync } from './utils/readLineAsync.js';
 import { Car } from './Car.js';
 
+const MAX_RACING_ROUND = 5;
+
 play();
 
 async function play() {
-  const name = await readLineAsync('자동차 이름을 입력하세요 > ');
+  const name = await readLineAsync('경주할 자동차 이름을 입력하세요.\n');
   const car = new Car(name);
 
-  console.log(`자동차 이름은 ${car.name}입니다.`);
+  console.log('\n실행 결과\n');
+  for (let currentRound = 1; currentRound <= MAX_RACING_ROUND; currentRound++) {
+    car.move();
+  }
+  console.log('경주를 완료했습니다.');
 }
