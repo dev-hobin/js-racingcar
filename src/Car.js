d4e2a1d3723b87a0cc9c67c4874121fbb3406d73
Hobin <66239735+dev-hobin@users.noreply.github.com>
2025-08-28 21:31:42 +0900

    feat: 자동차 이름 부여하고 5자 이하의 이름인지 검사하여 예외처리하기

--- src/Car.js
+++ src/Car.js
@@ -1,10 +1,21 @@
-export class Car { 
+const CAR_NAME_MAX_LENGTH = 5;
+
+export class Car {
   constructor(name) {
-      this.name = name;
-      this.location = 0;
+    if (!name) {
+      throw new Error('자동차 이름은 필수입니다.');
+    }
+    if (name.length > CAR_NAME_MAX_LENGTH) {
+      throw new Error(
+        `자동차 이름은 ${CAR_NAME_MAX_LENGTH}자 이하이어야 합니다.`,
+      );
+    }
+
+    this.name = name;
+    this.location = 0;
   }
 
   move() {
-      this.location += 1;
+    this.location += 1;
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import { readLineAsync } from './utils/readLineAsync.js';
+import { Car } from './Car.js';
 
 play();
 
 async function play() {
   const name = await readLineAsync('자동차 이름을 입력하세요 > ');
-  console.log(`자동차 이름은 ${name}입니다.`);
+  const car = new Car(name);
+
+  console.log(`자동차 이름은 ${car.name}입니다.`);
 }
