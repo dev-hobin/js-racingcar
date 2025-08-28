6a6ec5beb5a874630d87d0785cc96a685b6e9a40
Hobin <66239735+dev-hobin@users.noreply.github.com>
2025-08-28 22:08:01 +0900

    feat: 테스트 코드 추가

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -2,22 +2,35 @@ import { Car } from '../src/Car.js';
 
 const createCar = (name = '이름') => {
   return new Car(name);
-}
+};
 
 describe('자동차는', () => {
   it('이름을 상태로 가질 수 있다', () => {
-    const CAR_NAME = '이름'
-    
+    const CAR_NAME = '이름';
+
     const car = createCar(CAR_NAME);
 
     expect(car.name).toBe(CAR_NAME);
-  })
+  });
+
+  it('이름은 필수값이다', () => {
+    const ERROR_MESSAGE = '자동차 이름은 필수입니다.';
+
+    expect(() => createCar('')).toThrow(ERROR_MESSAGE);
+    expect(() => createCar('   ')).toThrow(ERROR_MESSAGE);
+  });
+
+  it('이름은 5자 이하만 가능하다.', () => {
+    const ERROR_MESSAGE = '자동차 이름은 5자 이하이어야 합니다.';
+
+    expect(() => createCar('이름이6글자')).toThrow(ERROR_MESSAGE);
+  });
 
   it('위치 값을 가지며, 초기 상태는 0 이다', () => {
     const car = createCar();
 
     expect(car.location).toBe(0);
-  })
+  });
 
   it('전진할 수 있으며 한 번에 1만큼 전진한다.', () => {
     const car = createCar();
@@ -25,5 +38,5 @@ describe('자동차는', () => {
     car.move();
 
     expect(car.location).toBe(1);
-  })
-})
+  });
+});
