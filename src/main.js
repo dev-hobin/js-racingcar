fde48617861cf08c5e5968494ca80f1d5f38bcfc
Hobin <66239735+dev-hobin@users.noreply.github.com>
2025-08-28 21:42:17 +0900

    chore: linebreak 유틸

--- src/main.js
+++ src/main.js
@@ -7,12 +7,19 @@ play();
 
 async function play() {
   const name = await readLineAsync('경주할 자동차 이름을 입력하세요.\n');
+
   const car = new Car(name);
 
-  console.log('\n실행 결과\n');
+  linebreak();
+  console.log('실행 결과');
   for (let currentRound = 1; currentRound <= MAX_RACING_ROUND; currentRound++) {
     car.move();
-    console.log(`${car.name} : ${'-'.repeat(car.location)}\n`);
+    console.log(`${car.name} : ${'-'.repeat(car.location)}`);
+    linebreak();
   }
   console.log('경주를 완료했습니다.');
+}
+
+function linebreak() {
+  console.log('\n');
 }
