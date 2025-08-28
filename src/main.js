44c35173682e3522ee4ca78d89b1ff345d6272dd
Hobin <66239735+dev-hobin@users.noreply.github.com>
2025-08-28 21:58:24 +0900

    feat: 자동차 이름은 쉼표(,)를 기준으로 구분하며, 여러 자동차를 만들 수 있다.

--- src/main.js
+++ src/main.js
@@ -6,15 +6,17 @@ const MAX_RACING_ROUND = 5;
 play();
 
 async function play() {
-  const name = await readLineAsync('경주할 자동차 이름을 입력하세요.\n');
+  const names = await readLineAsync('경주할 자동차 이름을 입력하세요.\n');
 
-  const car = new Car(name);
+  const cars = names.split(',').map((name) => new Car(name));
 
   linebreak();
   console.log('실행 결과');
   for (let currentRound = 1; currentRound <= MAX_RACING_ROUND; currentRound++) {
-    car.move();
-    console.log(`${car.name} : ${'-'.repeat(car.location)}`);
+    cars.forEach((car) => car.move());
+    cars.forEach((car) => {
+      console.log(`${car.name} : ${'-'.repeat(car.location)}`);
+    });
     linebreak();
   }
   console.log('경주를 완료했습니다.');
