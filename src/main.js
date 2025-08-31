3a5cab962fb69625ee7c163af3318aadc7d88f4d
Hobin <66239735+dev-hobin@users.noreply.github.com>
2025-08-31 21:44:58 +0900

    feat: 시도할 횟수 설정 기능 추가

--- src/main.js
+++ src/main.js
@@ -1,18 +1,17 @@
 import { readLineAsync } from './utils/readLineAsync.js';
 import { Car } from './models/Car/index.js';
 
-const MAX_RACING_ROUND = 5;
-
 play();
 
 async function play() {
   const names = await readLineAsync('경주할 자동차 이름을 입력하세요.\n');
+  const maxRound = await readLineAsync('시도할 회수는 몇회인가요?\n');
 
   const cars = names.split(',').map((name) => new Car(name));
 
   linebreak();
   console.log('실행 결과');
-  for (let currentRound = 1; currentRound <= MAX_RACING_ROUND; currentRound++) {
+  for (let currentRound = 1; currentRound <= maxRound; currentRound++) {
     moveCars(cars);
     printCurrentRound(cars);
   }
